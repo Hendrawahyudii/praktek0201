47246135dd0051f0be554fd0a9e7b173f6a7dcca
Hendrawahyudii <32281589+Hendrawahyudii@users.noreply.github.com>
2017-10-24 23:25:20 +0800

    Output bermacam gaya

--- praktek0202.cpp
+++ praktek0202.cpp
@@ -4,9 +4,13 @@ using namespace std;
 int main(){
 	int nilai=0;
 	int Nilai=1;
-	int NILai=2;
+	int NILAI=2;
 	int niLAi=3;
 	
 	double angka(10.5),Angka,ANGKA(3.14);
 	int a(10),b(7),c(12);
+	
+	cout << nilai<<" : "<<Nilai<<" | "<<NILAI<<" "<<niLAi<<endl;
+	cout << angka <<"|n";
+	cout << Angka <<endl<<ANGKA;
 }
